8e64ce5cd47acb237c57edf52f5a7aa9d439e4a5
Illuzzi66 <illuzzinati@gmail.com>
2026-02-14 14:00:02 -0800

    Update Layout.tsx to import and use logo image

---
+++ src/Layout.tsx
@@ -0,0 +1,9 @@
+import logo from '@/assets/screenshot_20260209-204916.png';
+
+// Other imports...
+
+// Inside your component render method
+// Replace <Coins className="w-8 h-8 text-gold" /> with:
+<img src={logo} alt="Logo" className="w-8 h-8" />
+
+// Rest of your Layout.tsx code...
